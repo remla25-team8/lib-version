752b5d539fe28bea1401976c5da37fcc629c5ea3
yongchenghuang <Y.Huang-51@student.tudelft.nl>
2025-05-03 12:28:31 +0200

    Fix: Fix the ci problem
    Fix the version number problem to pass the ci test.

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,6 @@
-from lib_version import print_version,get_version
+from lib_version import print_version, get_version
 import re
 
 def test_version_semver():
     print_version()
-    assert re.match(r"^\d+\.\d+\.\d+$", get_version())
+    assert re.match(r"^\d+\.\d+\.\d+(\.\w+)?$", get_version())
