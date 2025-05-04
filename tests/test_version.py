577edcd83274367ba34bc6df4e9600893865ce73
Lorenzo Sibi <l.sibi@student.tudelft.nl>
2025-05-04 18:04:09 +0200

    Typo check on test method. Possible to test it by just running `python -m tests.test_version`

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,11 @@
 from lib_version import VersionUtil
 import re
 
-def test_version_semver():
+def test_version_server():
     VersionUtil.print_version()
     assert re.match(r"^\d+\.\d+\.\d+(\.\w+)?$", VersionUtil.get_version())
+    
+    
+if __name__ == "__main__":
+    test_version_server()
+    print("All tests passed.")
