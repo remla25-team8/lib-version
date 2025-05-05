b413d0d62f2217d6f8e8a01375cdf4d835f32a1c
lsibi <l.sibi@student.tudelft.nl>
2025-05-05 22:09:16 +0200

    Revert "Release the code repo as published package"

--- lib_version/__init__.py
+++ lib_version/__init__.py
@@ -1,8 +1,1 @@
-from importlib.metadata import version, PackageNotFoundError
-
-try:
-    __version__ = version("lib_version")
-except PackageNotFoundError:
-    __version__ = "0.0.0"
-
 from .version_utils import VersionUtil
